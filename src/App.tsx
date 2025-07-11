55cb8181665ec660ca1018d4cfbb547d4129bfcf
Evison <evison.ndoni1@gmail.com>
2025-07-11 11:29:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex justify-center w-full  bg-black tetx-white h-screen">
+      jkdshf
+    </div>
+  );
+}
+
+export default App;
